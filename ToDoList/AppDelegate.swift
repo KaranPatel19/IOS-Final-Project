5ba8c58cb443c7a4fd019a704fee97af176a100a
karanpatel <A00264306@mycambrian.ca>
2023-04-17 01:19:33 -0400

    AppDelegate and SceneDelegate

---
+++ ToDoList/AppDelegate.swift
@@ -0,0 +1,13 @@
+//
+//  AppDelegate.swift
+//  ToDoList
+//
+//  Created by Karan Patel on 2023-04-15.
+//
+
+import UIKit
+import CoreData
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
---
+++ ToDoList/SceneDelegate.swift
@@ -0,0 +1,8 @@
+//
+//  SceneDelegate.swift
+//  ToDoList
+//
+//  Created by Karan Patel on 2023-04-15.
+//
+
+import UIKit
