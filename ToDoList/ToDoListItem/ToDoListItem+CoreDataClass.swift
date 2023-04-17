55538e2656afda88d63f9d4157182cb0b1110078
karanpatel <A00264306@mycambrian.ca>
2023-04-17 00:55:35 -0400

    Added CoreData
    Created a CoreData entity for a ToDoListItem with attributes for name and createdAt. Added Identifiable conformance. No changes to existing fetch request.

---
+++ ToDoList/ToDoListItem/ToDoListItem+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  ToDoListItem+CoreDataClass.swift
+//  ToDoList
+//
+//  Created by Karan Patel on 2023-04-15.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(ToDoListItem)
+public class ToDoListItem: NSManagedObject {
+
+}
