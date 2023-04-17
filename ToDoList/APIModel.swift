0eea09c9ce71de6e85b09c124dc1de076b485689
Vishal <vishalvsutar@gmail.com>
2023-04-17 01:15:22 -0400

    Created a new APIModel class conforming to Codable protocol, containing properties that represent different aspects of an activity fetched from an API.

---
+++ ToDoList/APIModel.swift
@@ -0,0 +1,16 @@
+//
+//  APIModel.swift
+//  ToDoList
+//
+//  Created by Vishal Sutar on 2023-04-17.
+//
+
+class APIModel: Codable {
+    let accessibility: Double
+    let activity: String
+    let key: String
+    let link: String
+    let participants: Int
+    let price: Double
+    let type: String
+}
