7e68d15a5b7d7dc5757d48a7f7a76063bbe3e29b
karanpatel <A00264306@mycambrian.ca>
2023-04-17 01:25:51 -0400

    Updating remaining files, and submitting all the codes.

---
+++ ToDoList/ToDoListItem/ToDoListItem+CoreDataProperties.swift
@@ -0,0 +1,26 @@
+//
+//  ToDoListItem+CoreDataProperties.swift
+//  ToDoList
+//
+//  Created by Karan Patel on 2023-04-15.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension ToDoListItem {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<ToDoListItem> {
+        return NSFetchRequest<ToDoListItem>(entityName: "ToDoListItem")
+    }
+
+    @NSManaged public var name: String?
+    @NSManaged public var createdAt: Date?
+
+}
+
+extension ToDoListItem : Identifiable {
+
+}
